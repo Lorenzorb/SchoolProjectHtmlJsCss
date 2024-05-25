7d9559140d4c7e8c4ff83b96f5a7937a534ae6c1
Lorenzo Ramos <52989967+Lorenzorb@users.noreply.github.com>
2024-05-25 10:36:29 -0700

    asdasd

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ function toggleSelected(button) {
   clearSelected();
   console.log("Pressed", button.id);
   button.classList.toggle("selected");
+  document.getElementById(button.id).style.backgroundColor='Yellow';
 }
 
 function clearSelected() {
   var buttons = document.querySelectorAll(".button-multiple-choice");
   buttons.forEach((button) => {
+    document.getElementById(button.id).style.backgroundColor='White';
     button.classList.remove("selected");
   });
 }
@@ -21,24 +23,40 @@ function clearSelected() {
 function checkMultipleChoiceAnswer() {
   var buttons = document.querySelectorAll(".button-multiple-choice");
   console.log("check answer");
-  buttonC.style.backgroundColor = "green";
   buttons.forEach((button) => {
     if (button.classList.contains("selected")) {
       console.log(button.id);
 
       if (button.id == "button-c") {
+        document.getElementById(button.id).style.backgroundColor='Green';
         console.log("yup");
+      } else {
+        document.getElementById(button.id).style.backgroundColor='Red';
       }
     }
   });
 }
 
 function checkFreeResponseAnswer(id) {
+  
   var response = document.getElementById(id).value;
-  if (response == answer) {
+  console.log(response);
+  if (response.toLowerCase() == "grief") {
+    document.getElementById('input-response').style.borderColor='Green';
+    revealCorrectResponse()
+  } else {
+    document.getElementById('input-response').style.borderColor='Red';
   }
 }
 
 function clearTextBox(input) {
+  document.getElementById('input-response').style.borderColor='White';
   input.value = "";
+}
+
+
+function revealCorrectResponse() {
+  var element = document.getElementById("h3-free-response-text");
+  correctResponse = "I sat with my anger long enough until she told me her real name was grief";
+  element.textContent = correctResponse;
 }
